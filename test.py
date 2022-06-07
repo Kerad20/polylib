f4f5b1a5ba5215551d787f059e80ed3a65754a13
Darek Libecki <dariuszlibecki@gmail.com>
2022-06-07 22:25:49 +0200

    Add parameterized tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import intpolylib
+from parameterized import parameterized
 
 class ParametrizedTestCase(unittest.TestCase):
     """ TestCase classes that want to be parametrized should
@@ -23,85 +24,31 @@ class ParametrizedTestCase(unittest.TestCase):
 
 class TestFindDivisors(ParametrizedTestCase): # testy funkcji znajdującej dzielniki
 
-    def test1(self): # sprawdzanie wybranej dużej liczby wiekszej od 2^16
-        data = 1048576
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
-    
-    def test2(self): # sprawdzanie działania dla liczby 2^32
-        data = 4294967296
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 
-        2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
+    @parameterized.expand([
+        ["test1", 1048576, [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576]],
+        ["test2", 4294967296, [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 
+        2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 4294967296]],
+        ["test3", 5247145, [1, 5, 1049429, 5247145]],
+        ["test4", 25000, [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 625, 1000, 1250, 2500, 3125, 5000, 6250, 12500, 25000]],
+        ["test5", 20988580, [1, 2, 4, 5, 10, 20, 1049429, 2098858, 4197716, 5247145, 10494290, 20988580]],
+        ["test6", 9064457, [1, 9064457]],
+        ["test7", 1, [1]],
+        ["test9", 2183462912, [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 33317, 65536, 66634, 133268, 
+        266536, 533072, 1066144, 2132288, 4264576, 8529152, 17058304, 34116608, 68233216, 136466432, 272932864, 545865728, 1091731456, 2183462912]]
+    ])
 
-    def test3(self): # duża liczba zawierająca dzielnik będący liczbą pierwszą większą niż 2^16
-        data = 5247145
+    def test_divisors(self, name, data, correct_answer):
         base = intpolylib.BasePolynomial()
         result = base.findDivisors(data)
-        correctAnswer = [1, 5, 1049429, 5247145]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
-
-    def test4(self): # liczba mniejsza niż 2^16
-        data = 25000
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 625, 1000, 1250, 2500, 3125, 5000, 6250, 12500, 25000]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
-    
-    def test5(self): # liczba > 2^16 zawierająca małe dzielniki jak i duże dzielniki pierwsze
-        data = 20988580
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1, 2, 4, 5, 10, 20, 1049429, 2098858, 4197716, 5247145, 10494290, 20988580]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
-
-    def test6(self): # faktoryzacja liczby pierwszej > 2^16
-        data = 9064457
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1, 9064457]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
-
-    def test7(self): # faktoryzacja 1
-        data = 1
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
-        self.assertEqual(result, correctAnswer)
+        minus = [-i for i in correct_answer]
+        correct_answer = sorted(correct_answer + minus)
+        self.assertEqual(result, correct_answer)
 
     def test8(self): # faktoryzacja 0
         data = 0
         base = intpolylib.BasePolynomial()
         result = base.findDivisors(data)
         correctAnswer = []
-        self.assertEqual(result, correctAnswer)
-
-    def test9(self): # liczba z dużą ilością dzielników
-        data = 2183462912
-        base = intpolylib.BasePolynomial()
-        result = base.findDivisors(data)
-        correctAnswer = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 33317, 65536, 66634, 133268, 
-        266536, 533072, 1066144, 2132288, 4264576, 8529152, 17058304, 34116608, 68233216, 136466432, 272932864, 545865728, 1091731456, 2183462912]
-        minus = [-i for i in correctAnswer]
-        correctAnswer = sorted(correctAnswer + minus)
         self.assertEqual(result, correctAnswer)
 
     def test10(self): # testy dzielników mniejszych liczb
@@ -115,48 +62,30 @@ class TestFindDivisors(ParametrizedTestCase): # testy funkcji znajdującej dziel
                 self.assertEqual(correctAnswer, result)
 
 class TestFactorization(ParametrizedTestCase): # testy faktoryzacji
-    
-    def test1(self): # testy faktoryzacji bez pierwiastków
-        poly = self.param("x^5-x^4-2x^3-8x^2+6x-1")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ["x^2-3x+1", "x^3+2x^2+3x-1"]
-        self.assertEqual(result, correctAnswer)
 
-    def test2(self): # testy faktoryzacji z pierwiastkami oraz jednyn nierozkładalnym czynnikiem
-        poly = self.param("x^4+x^2-20")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ["x-2", "x+2", "x^2+5"]
-        self.assertEqual(result, correctAnswer)
+    @parameterized.expand([
+        ["test1", "x^5-x^4-2x^3-8x^2+6x-1", ["x^2-3x+1", "x^3+2x^2+3x-1"]],
+        ["test2", "x^4+x^2-20", ["x-2", "x+2", "x^2+5"]],
+        ["test3", "14x^4-46x^3-82x^2+138x+120", ['7x+5', 'x-4', '2x^2-6']],
+        ["test4", "x^6+3x^3-4", ["x-1", "x^2+x+1", "x^3+4"]],
+        ["test5", "2x^14-512x^6", ["x", "x", "x", "x", "x", "x", "x-2", "x+2", "x^2+4", "2x^4+32"]],
+        ["test6", "-7x^8-x^5+x^3-x^2-8x-1", ["-7x^8-x^5+x^3-x^2-8x-1"]],
+        ["test8", "6x^7+3x^4-9x^3", ["x", "x", "x","x-1", "6x^3+6x^2+6x+9"]],
+        ["test9", "7x+7x^3+x^4+x^6", ['x', 'x^2+1', 'x^3+7']],
+        ["test10", "x^2-20x+100", ['x-10', 'x-10']],
+        ["test11", "x^4-25", ['-x^2+5', '-x^2-5']],
+        ["test12", "3x^4-3x^3-36x^2", ['x', 'x', 'x-4','x+3', '3']],
+        ["test13", "x^4+x^2-20", ['x-2', 'x+2', 'x^2+5']],
+        ["test14", "6x^6+x^3-2", ['-2x^3+1', '-3x^3-2']],
+        ["test15", "2x^8-14x^4+20", ['-x^4+2', '-2x^4+10']],
+        ["test16", "3x^5-17x^4-28x^3", ['x', 'x', 'x', 'x-7', '3x+4']]
+    ])
 
-    def test3(self): # testy faktoryzacji z dużą nierozkładalną częścią w dziedzinie liczb całkowitych i jednym pierwiastkiem
-        poly = self.param("14x^4-46x^3-82x^2+138x+120")
+    def test_factorization(self, name, poly_str, correct_answer): # testy faktoryzacji bez pierwiastków
+        poly = self.param(poly_str)
         factors = poly.factorization()
         result = [str(factor) for factor in factors]
-        correctAnswer = ['7x+5', 'x-4', '2x^2-6']
-        self.assertEqual(result, correctAnswer)
-
-    def test4(self): # faktoryzacja wielomianu wyższego rzędu, zawierjącego pierwiastek jak i części nierozkładalne
-        poly = self.param("x^6+3x^3-4")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ["x-1", "x^2+x+1", "x^3+4"]
-        self.assertEqual(result, correctAnswer)
-
-    def test5(self): # faktoryzacja wielomianu wysokiego rzędu z wyrazem wolnym 0
-        poly = self.param("2x^14-512x^6")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ["x", "x", "x", "x", "x", "x", "x-2", "x+2", "x^2+4", "2x^4+32"]
-        self.assertEqual(result, correctAnswer)
-
-    def test6(self): # wielomian nierozkładalny
-        poly = self.param("-7x^8-x^5+x^3-x^2-8x-1")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ["-7x^8-x^5+x^3-x^2-8x-1"]
-        self.assertEqual(result, correctAnswer)
+        self.assertEqual(result, correct_answer)
 
     def test7(self): # wielokrotny pierwiastek
         poly = self.param("x+4")
@@ -166,180 +95,49 @@ class TestFactorization(ParametrizedTestCase): # testy faktoryzacji
         correctAnswer = ["x+4", "x+4", "x+4", "x+4"]
         self.assertEqual(result, correctAnswer)
 
-    def test8(self): # pierwiastek zero wielokrotny i jeden zwykły pierwiastek
-        poly = self.param("6x^7+3x^4-9x^3")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ["x", "x", "x","x-1", "6x^3+6x^2+6x+9"]
-        self.assertEqual(result, correctAnswer)
-
-    def test9(self):
-        poly = self.param("7x+7x^3+x^4+x^6")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['x', 'x^2+1', 'x^3+7']
-        self.assertEqual(result, correctAnswer)
-
-    def test10(self): # pierwiastek wielokrotny
-        poly = self.param("x^2-20x+100")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['x-10', 'x-10']
-        self.assertEqual(result, correctAnswer)
-
-    def test11(self):
-        poly = self.param("x^4-25")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['-x^2+5', '-x^2-5']
-        self.assertEqual(result, correctAnswer)
-
-    def test12(self):
-        poly = self.param("3x^4-3x^3-36x^2")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['x', 'x', 'x-4','x+3', '3']
-        self.assertEqual(result, correctAnswer)
-
-    def test13(self):
-        poly = self.param("x^4+x^2-20")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['x-2', 'x+2', 'x^2+5']
-        self.assertEqual(result, correctAnswer)
-
-    def test14(self):
-        poly = self.param("6x^6+x^3-2")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['-2x^3+1', '-3x^3-2']
-        self.assertEqual(result, correctAnswer)     
-
-    def test15(self):
-        poly = self.param("2x^8-14x^4+20")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['-x^4+2', '-2x^4+10']
-        self.assertEqual(result, correctAnswer)     
-
-    def test16(self):
-        poly = self.param("3x^5-17x^4-28x^3")
-        factors = poly.factorization()
-        result = [str(factor) for factor in factors]
-        correctAnswer = ['x', 'x', 'x', 'x-7', '3x+4']
-        self.assertEqual(result, correctAnswer)        
-    
-
 class TestDerivative(ParametrizedTestCase): # testy pochodnej
 
-    def test1(self):
-        poly = self.param("x^3+x^2-10x+8")
-        result = str(poly.derivative())
-        correctAnswer = "3x^2+2x-10"
-        self.assertEqual(result, correctAnswer)
-    
-    def test2(self):
-        poly = self.param("x^4+2x^3-13x^2+4x-30")
-        result = str(poly.derivative())
-        correctAnswer = "4x^3+6x^2-26x+4"
-        self.assertEqual(result, correctAnswer)
+    @parameterized.expand([
+        ["test1", "x^3+x^2-10x+8", "3x^2+2x-10"],
+        ["test2", "x^4+2x^3-13x^2+4x-30", "4x^3+6x^2-26x+4"],
+        ["test3", "x^8+2x^7+x^6", "8x^7+14x^6+6x^5"],
+        ["test4", "x-1", "1"],
+        ["test5", "2x^3-5x^2+4x+1", "6x^2-10x+4"],
+        ["test6", "5", "0"]
+    ])
 
-    def test3(self):
-        poly = self.param("x^8+2x^7+x^6")
+    def test_derivative(self, name, poly_str, correct_answer):
+        poly = self.param(poly_str)
         result = str(poly.derivative())
-        correctAnswer = "8x^7+14x^6+6x^5"
-        self.assertEqual(result, correctAnswer)
-
-    def test4(self): # pochodna małego wyrażenia
-        poly = self.param("x-1")
-        result = str(poly.derivative())
-        correctAnswer = "1"
-        self.assertEqual(result, correctAnswer)
-
-    def test5(self):
-        poly = self.param("2x^3-5x^2+4x+1")
-        result = str(poly.derivative())
-        correctAnswer = "6x^2-10x+4"
-        self.assertEqual(result, correctAnswer)   
-
-    def test6(self): # pochodna stałej
-        poly = self.param("5")
-        result = str(poly.derivative())
-        correctAnswer = "0"
-        self.assertEqual(result, correctAnswer)   
+        self.assertEqual(result, correct_answer) 
 
 class TestStr(ParametrizedTestCase): # testy zamiany wielomianu z str na listę i z listy na str
-    def test1(self):
-        poly = self.param("x-2")
+
+    @parameterized.expand([
+        ["test1", "x-2", "x-2"],
+        ["test2", "x^10-12457x^4-3x^3+2", "x^10-12457x^4-3x^3+2"],
+        ["test3", "-x^99-1", "-x^99-1"],
+        ["test4", "0", "0"],
+        ["test5", "12345779", "12345779"],
+        ["test6", "-88x", "-88x"],
+        ["test7", "-x^105", "-x^105"],
+        ["test8", "x+x^100-7-x^2+80x^3", "x^100+80x^3-x^2+x-7"]
+    ])
+
+    def test_str(self, name, poly_str, correct_answer):
+        poly = self.param(poly_str)
         result = str(poly)
-        correctAnswer = "x-2"
-        self.assertEqual(result, correctAnswer)
+        self.assertEqual(result, correct_answer)
 
-    def test2(self):
-        poly = self.param("x^10-12457x^4-3x^3+2")
-        result = str(poly)
-        correctAnswer = "x^10-12457x^4-3x^3+2"
-        self.assertEqual(result, correctAnswer)
+    @parameterized.expand([
+        ["test1", "x^10+2x^10"],
+        ["test2", "x-5x"],
+        ["test3", "3+5"]
+    ])
 
-    def test3(self):
-        poly = self.param("-x^99-1")
-        result = str(poly)
-        correctAnswer = "-x^99-1"
-        self.assertEqual(result, correctAnswer)
-
-    # def test4(self):
-    #     poly = self.param("-500000x^1000+345x^500-45x^45+12x^10-76x^8+x^3-1")
-    #     result = str(poly)
-    #     correctAnswer = "-500000x^1000+345x^500-45x^45+12x^10-76x^8+x^3-1"
-    #     self.assertEqual(result, correctAnswer)     
-
-    def test5(self):
-        poly = self.param("0")
-        result = str(poly)
-        correctAnswer = "0"
-        self.assertEqual(result, correctAnswer)    
-
-    def test6(self):
-        poly = self.param("12345779")
-        result = str(poly)
-        correctAnswer = "12345779"
-        self.assertEqual(result, correctAnswer)   
-
-    def test7(self):
-        poly = self.param("-88x")
-        result = str(poly)
-        correctAnswer = "-88x"
-        self.assertEqual(result, correctAnswer)   
-
-    def test8(self):
-        poly = self.param("-88x")
-        result = str(poly)
-        correctAnswer = "-88x"
-        self.assertEqual(result, correctAnswer)  
-
-    def test9(self):
-        poly = self.param("-x^105")
-        result = str(poly)
-        correctAnswer = "-x^105"
-        self.assertEqual(result, correctAnswer) 
-
-    def test10(self): # jednomiany w różnej kolejności
-        poly = self.param("x+x^100-7-x^2+80x^3")
-        result = str(poly)
-        correctAnswer = "x^100+80x^3-x^2+x-7" # na wyjściu posortowane jednomiany
-        self.assertEqual(result, correctAnswer) 
-
-    def test11(self): # wielomiany muszą mieć różne potęgi jednomianów
+    def test_errors(self, name, poly_str): # wielomiany muszą mieć różne potęgi jednomianów
         with self.assertRaises(ValueError):
-            self.param("x^10+2x^10")
-
-    def test12(self): # wielomiany muszą mieć różne potęgi jednomianów
-        with self.assertRaises(ValueError):
-            self.param("x-5x")
-
-    def test13(self): # wielomiany muszą mieć różne potęgi jednomianów
-        with self.assertRaises(ValueError):
-            self.param("3+5")
+            self.param(poly_str)
 
 class TestBasicOperations(ParametrizedTestCase):
     def test1(self): # odejmowanie tych samych wielomianów
